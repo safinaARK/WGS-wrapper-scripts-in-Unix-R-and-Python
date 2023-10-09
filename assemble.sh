e1a26a7219002db947dc22ab8bff45789f138d40
sidratahir1 <140487970+sidratahir1@users.noreply.github.com>
2023-10-09 15:49:44 +0500

    Update assemble.sh

--- assemble.sh
+++ assemble.sh
@@ -2,7 +2,9 @@
 
 #DE NOVO ASSEMBLY WITH SPADES
 
-for F in trim_fastq/*_1.trim.fastq.gz; do   R=${F/_1/_2};   out=$(basename ${F%%_1.trim.fastq.gz});   output_dir="${out}_assemble";   echo spades.py --careful -o $output_dir -1 $F -2 $R; done
+for F in trim_fastq/*_1.trim.fastq.gz; do   R=${F/_1/_2};   
+out=$(basename ${F%%_1.trim.fastq.gz});   output_dir="${out}_assemble";  
+spades.py --careful -o $output_dir -1 $F -2 $R; done
 
 
 
